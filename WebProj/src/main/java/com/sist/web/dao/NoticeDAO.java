241cc58259f3887cda4b946a00b80178c678da1c
qwerty <qwerty@asdf>
2017-11-02 15:09:01 +0900

    우종아... 나 성열이...

--- WebProj/src/main/java/com/sist/web/dao/NoticeDAO.java
+++ WebProj/src/main/java/com/sist/web/dao/NoticeDAO.java
@@ -8,6 +8,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
 
 import com.sist.web.vo.Notice;
@@ -17,14 +21,17 @@ import com.sist.web.vo.Notice;
 @Repository
 public class NoticeDAO {
 
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@211.238.142.132:1521:orcl";
+	String user = "JOINUS";
+	String pwd = "TJDDUF";
+	
+	@Autowired
+	private JdbcTemplate jt;
+	
 
 	// 而⑤꽖�뀡 �뿰寃�
 	public Connection getConn(){
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@211.238.142.132:1521:orcl";
-		//private String url = "jdbc:oracle:thin:@211.238.142.15:1521:orcl";
-		String user = "JOINUS";
-		String pwd = "TJDDUF";
 
 		Connection con = null;
 		try {
@@ -69,10 +76,8 @@ public class NoticeDAO {
 	}
 	
 	
-	//寃뚯떆湲� 媛��닔 �뼸湲�
 	public int getSeqCount(String field, String query){
 		Connection con = null;
-		//Statement st=null;
 		PreparedStatement ps=null;
 		ResultSet rs = null;
 		int count=0;
@@ -81,8 +86,6 @@ public class NoticeDAO {
 		con=getConn();
 
 		try {
-			/*st = con.createStatement();
-			rs=st.executeQuery(sql);*/
 			ps = con.prepareStatement(sql);
 			ps.setString(1, "%"+query+"%");
 			rs=ps.executeQuery();
@@ -107,7 +110,6 @@ public class NoticeDAO {
 		return count;
 	}
 
-	// 寃뚯떆湲� 紐⑸줉 議고쉶
 	public List<Notice> getNotices(int pg, String field, String query){
 		System.out.println("pg="+pg);
 		System.out.println("field="+field);
@@ -119,16 +121,13 @@ public class NoticeDAO {
 		ResultSet rs = null;
 		List<Notice> list = null;
 		
-		int startRN = 1 + (pg-1)*10;   // �븳 �럹�씠吏��뿉 �몴�떆�맆 湲� 媛��닔
+		int startRN = 1 + (pg-1)*10;   
 
 		String sql = "SELECT * FROM (SELECT ROWNUM NUM, N.* FROM (SELECT * FROM NOTICES WHERE "+field+" LIKE ? ORDER BY TO_NUMBER(SEQ) DESC) N) WHERE NUM BETWEEN ? AND ?";
 		con=getConn();
 		try {
-			/*st = con.createStatement();
-			rs=st.executeQuery(sql);*/
 			
 			ps = con.prepareStatement(sql);
-			//ps.setString(1, query);
 			ps.setString(1, "%"+query+"%");
 			ps.setInt(2, startRN);
 			ps.setInt(3, startRN+10);
@@ -136,7 +135,7 @@ public class NoticeDAO {
 
 			list= new ArrayList<Notice>();
 			while(rs.next()){
-				Notice n = new Notice();   // 媛앹껜�쓽 �깮�꽦 �쐞移� 留ㅼ슦 以묒슂
+				Notice n = new Notice();   
 				n.setSeq(rs.getString("seq"));
 				n.setTitle(rs.getString("title"));
 				n.setWriter(rs.getString("writer"));
@@ -165,46 +164,23 @@ public class NoticeDAO {
 		return list;
 	}
 
-	//寃뚯떆湲�蹂닿린
-	public Notice getNotice(String seq){    // �떆利�2�뿉�꽌 �궗�슜
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		Notice n=null;
-
+	public Notice getNotice(String seq){    
+		System.out.println("jdbcTemplate 사용해서 getNotice() 호출");
+//		JdbcTemplate jt = new JdbcTemplate();
+//		DriverManagerDataSource ds = new DriverManagerDataSource();
+//		ds.setDriverClassName(driver);
+//		ds.setUrl(url);
+//		ds.setUsername(user);
+//		ds.setPassword(pwd);
+//		
+//		jt.setDataSource(ds);
+//		
 		String sql = "SELECT * FROM NOTICES WHERE SEQ=?";
-		con = getConn();
-		try {
-			ps=con.prepareStatement(sql);
-			ps.setString(1, seq);
-			rs=ps.executeQuery();   //議고쉶�씤 SELECT 臾몄뿉�꽌留� �궗�슜
-			if(rs.next()){
-				n = new Notice();    // 媛앹껜�쓽 �깮�꽦 �쐞移� 留ㅼ슦 以묒슂
-				n.setSeq(rs.getString("seq"));
-				n.setTitle(rs.getString("title"));
-				n.setWriter(rs.getString("writer"));
-				n.setContent(rs.getString("content"));
-				n.setRegdate(rs.getString("regdate"));
-				n.setHit(rs.getInt("hit"));
-				n.setFileSrc(rs.getString("filesrc"));
-			}
-		} catch (SQLException e) {
-			System.out.println("�쉶�썝 議고쉶以� �삤瑜섍� 諛쒖깮�븯���뒿�땲�떎.");
-			e.printStackTrace();
-		} finally {
-			try {
-				rs.close();
-				ps.close();
-				con.close();
-			} catch (SQLException e) {
-				System.out.println("�젒�냽�빐�젣�뿉 �떎�뙣�븯���뒿�땲�떎.");
-				e.printStackTrace();
-			}
-		}
-		return n;
+		return jt.queryForObject(sql, new Object[] {seq}, new BeanPropertyRowMapper<Notice>(Notice.class));
+		
+
 	}
 
-	//寃뚯떆湲� �닔�젙
 	public int updateNotice(Notice n) {
 		Connection con = null;
 		PreparedStatement ps=null;
@@ -239,7 +215,6 @@ public class NoticeDAO {
 		return af;
 	}
 
-	//寃뚯떆湲� �궘�젣
 	public int delNotice(String seq) {
 		Connection con = null;
 		PreparedStatement ps=null;
@@ -268,7 +243,6 @@ public class NoticeDAO {
 		return af;
 	}
 
-	//寃뚯떆湲� �벑濡�
 	public int insertNotice(Notice n){
 		Connection con = null;
 		PreparedStatement ps=null;
@@ -284,9 +258,6 @@ public class NoticeDAO {
 			ps.setString(4, n.getFileSrc());
 
 			af = ps.executeUpdate();
-			/*if(af==1){
-				System.out.println("�쉶�썝異붽��뿉 �꽦怨듯븯���뒿�땲�떎.");
-			}*/
 		} catch (SQLException e) {
 			System.out.println("寃뚯떆湲� �벑濡앹쨷�삤瑜섍� 諛쒖깮�븯���뒿�땲�떎.");
 			e.printStackTrace();
